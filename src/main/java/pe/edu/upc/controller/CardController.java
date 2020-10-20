1b56b92d4c7b69813bc6d9c617f5f7a5719fa24e
NicolasFer100 <NicolasFer100>
2020-10-20 10:33:42 -0500

    Avance de los controller

---
+++ src/main/java/pe/edu/upc/controller/CardController.java
@@ -0,0 +1,58 @@
+package pe.edu.upc.controller;
+
+import javax.validation.Valid; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import pe.edu.upc.entity.Card;
+import pe.edu.upc.serviceinterface.ICardService;
+
+@Controller
+@RequestMapping("/cards")
+public class CardController {
+
+	@Autowired
+	private ICardService cS;
+	
+	@GetMapping("/new")
+	public String newLBrand(Model model) {
+		model.addAttribute("card", new Card());
+		return "card/card";
+
+	}
+	
+	@PostMapping("/save")
+	public String saveBrand(@Valid Card card, BindingResult result, Model model, 
+			SessionStatus status ) throws Exception {
+		
+		if (result.hasErrors()) {
+			
+			return "card/card";
+		}else {
+			cS.insert(card);
+		}
+		model.addAttribute("listaCard", cS.list());
+		
+		return "/card/listCard";
+		
+	}
+	
+	@GetMapping("/list")
+	public String listCard(Model model) {
+		
+		try {
+			model.addAttribute("listaTarjetas", cS.list());
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error al listar en el controller");
+		}
+		return "card/listCard";
+	}
+}
---
+++ src/main/java/pe/edu/upc/controller/DistrictController.java
@@ -0,0 +1,58 @@
+package pe.edu.upc.controller;
+
+import javax.validation.Valid; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import pe.edu.upc.entity.District;
+import pe.edu.upc.serviceinterface.IDistrictService;
+
+@Controller
+@RequestMapping("/districts")
+public class DistrictController {
+
+	@Autowired
+	private IDistrictService dS;
+	
+	@GetMapping("/new")
+	public String newLDistrict(Model model) {
+		model.addAttribute("district", new District());
+		return "district/district";
+
+	}
+	
+	@PostMapping("/save")
+	public String saveBrand(@Valid District district, BindingResult result, Model model, 
+			SessionStatus status ) throws Exception {
+		
+		if (result.hasErrors()) {
+			
+			return "district/district";
+		}else {
+			dS.insert(district);
+		}
+		model.addAttribute("listaDistrict", dS.list());
+		
+		return "/district/listDistrict";
+		
+	}
+	
+	@GetMapping("/list")
+	public String listDistrict(Model model) {
+		
+		try {
+			model.addAttribute("listaDistritos", dS.list());
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error al listar en el controller");
+		}
+		return "district/listDistrict";
+	}
+}
