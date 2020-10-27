253ad7b962f9cd8e6ce0c74bae444561b9c2a99c
giova977 <71194377+giova977@users.noreply.github.com>
2020-10-27 03:15:35 -0500

    correccion

--- src/main/java/pe/edu/upc/entity/Store.java
+++ src/main/java/pe/edu/upc/entity/Store.java
@@ -1,12 +1,16 @@
 package pe.edu.upc.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name ="stores")
 public class Store {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
