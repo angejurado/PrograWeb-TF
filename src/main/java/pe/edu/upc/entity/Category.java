98bd2871c88a1aab0d5e38aed191281143394ee9
NicolasFer100 <NicolasFer100>
2020-10-27 02:22:36 -0500

    Correcion

--- src/main/java/pe/edu/upc/entity/Category.java
+++ src/main/java/pe/edu/upc/entity/Category.java
@@ -1,12 +1,10 @@
 package pe.edu.upc.entity;
 
-import javax.persistence.Column;
+import javax.persistence.Column; 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
